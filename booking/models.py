141bc247c7cd5862e93b9d74fadb8a200f6f4925
Mahsak89 <>
2023-08-05 22:19:01 +0000

    added SERVICE_CHOICES and TIME_CHOICES in model in booking

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
-# Create your models here.
+SERVICE_CHOICES = (
+    ("Business Strategy Development", "Business Strategy Development"),
+    ("Financial Planning and Analysis", "Financial Planning and Analysis"),
+    ("Market Research and Analysis", "Market Research and Analysis"),
+    ("Process Optimization", "Process Optimization"),
+    ("Leadership Development", "Leadership Development"),
+    ("Change Management", "Change Management"),
+    ("consulting", "consulting"),
+)
+TIME_CHOICES = (
+    ("3 PM", "3 PM"),
+    ("4 PM", "4 PM"),
+    ("5 PM", "5 PM"),
+)
