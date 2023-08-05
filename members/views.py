e782a1ba7fc0ef8e284975935af840c383b324b4
Mahsak89 <>
2023-08-05 19:53:43 +0000

    added register_user url and function in views and added the html template

--- members/views.py
+++ members/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+
 
 
 def login_user(request):
@@ -8,7 +10,7 @@ def login_user(request):
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
-        if user is not None: 
+        if user is not None:
             login(request, user)
             # Return an ' login'success message.
             messages.success(
@@ -29,3 +31,21 @@ def logout_user(request):
     logout(request)
     messages.success(request, "Sign Out Successful")
     return redirect('index')
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "Sign Up Completed!")
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'authenticate/register_user.html', {
+        'form': form,
+    })
