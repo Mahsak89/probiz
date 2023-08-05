053fb5b49b6da4f57730959f29621c72d0b42814
Mahsak89 <>
2023-08-05 22:17:11 +0000

    added to style.css some initial style and navbar and footer

--- probiz/settings.py
+++ probiz/settings.py
@@ -90,9 +90,17 @@ WSGI_APPLICATION = 'probiz.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
