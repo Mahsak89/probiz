a75db9b1d5c4a8a15bc10bb725e359bc1040cfcc
Mahsak89 <>
2023-08-05 22:26:55 +0000

    added appointment class in model

--- booking/models.py
+++ booking/models.py
@@ -16,3 +16,17 @@ TIME_CHOICES = (
     ("4 PM", "4 PM"),
     ("5 PM", "5 PM"),
 )
+
+
+class Appointment(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True, blank=True)
+    service = models.CharField(
+        max_length=50, choices=SERVICE_CHOICES, default="Business Strategy Development")
+    day = models.DateField(default=datetime.now)
+    time = models.CharField(
+        max_length=10, choices=TIME_CHOICES, default="3 PM")
+    time_ordered = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} | day: {self.day} | time: {self.time}"
