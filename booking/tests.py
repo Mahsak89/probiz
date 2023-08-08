cfeb17b506f61e46fa1045b4f4e8db6596205419
Mahsak89 <>
2023-08-08 11:28:09 +0000

    added userupdatesubmitviewtest

--- booking/tests.py
+++ booking/tests.py
@@ -48,3 +48,23 @@ class UserUpdateViewTest(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'userUpdate.html')
+
+
+class UserUpdateSubmitViewTest(TestCase):
+    def setUp(self):
+        # Create a test user
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword'
+        )
+        # Create a test appointment
+        self.appointment = Appointment.objects.create(
+            user=self.user, service='Test Service', day=date.today(), time='3 PM'
+        )
+
+    def test_user_update_submit_view_get(self):
+        url = reverse('userUpdateSubmit', args=[self.appointment.id])
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'userUpdateSubmit.html')
