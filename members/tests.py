70da85a2e57aaca66221fc15cb6ec2f53d5f6765
Mahsak89 <>
2023-08-08 11:41:37 +0000

    test logoutuserview

--- members/tests.py
+++ members/tests.py
@@ -27,3 +27,22 @@ class LoginUserViewTest(TestCase):
 
         # Check if the user is logged in
         self.assertTrue('_auth_user_id' in self.client.session)
+
+
+class LogoutUserViewTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword'
+        )
+        self.logout_url = reverse('logout')
+
+    def test_logout_user(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(self.logout_url)
+
+        # Redirects to 'index' page after successful logout
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('index'))
+
+        # Check if the user is logged out
+        self.assertFalse('_auth_user_id' in self.client.session)
