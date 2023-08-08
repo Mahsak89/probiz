af95e42e63bae6c767f7b99c3011c2343489a039
Mahsak89 <>
2023-08-08 11:39:56 +0000

    test loginuserview

--- members/tests.py
+++ members/tests.py
@@ -1,1 +1,29 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+
+class LoginUserViewTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword'
+        )
+        self.login_url = reverse('login')
+
+    def test_login_user_get(self):
+        response = self.client.get(self.login_url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'authenticate/login.html')
+
+    def test_login_user_post_valid(self):
+        data = {'username': 'testuser', 'password': 'testpassword'}
+        response = self.client.post(self.login_url, data)
+
+        # Redirects to 'index' page after successful login
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('index'))
+
+        # Check if the user is logged in
+        self.assertTrue('_auth_user_id' in self.client.session)
