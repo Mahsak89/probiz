76d323364d87b944e234f6a7c843d7340f8f83bc
Mahsak89 <>
2023-08-09 10:00:46 +0000

    change debug to fix

--- probiz/settings.py
+++ probiz/settings.py
@@ -29,8 +29,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-X_FRAME_OPTIONS = 'SAMEORIGIN'
+DEBUG = True
+
 
 
 ALLOWED_HOSTS = ['probizz-bd3dbf6d99f9.herokuapp.com', 'localhost',
